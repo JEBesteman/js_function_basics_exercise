74e1e2d2023c9cec5e379e287be56a67ab0d1954
JEBesteman <86465468+JEBesteman@users.noreply.github.com>
2021-08-01 14:52:44 +0200

    Add files via upload

---
+++ function_basics_exercise/script.js
@@ -0,0 +1,33 @@
+const makePizza = function () {
+    console.log("Rol het deeg uit");
+    console.log("Verdeel de saus over de pizza");
+    console.log("Verdeel de salami, kaas, pepers, enz over de pizza");
+    console.log("Stop de pizza in de voorverwarmde oven");
+    console.log("Haal de pizza er na 30 min uit");
+}
+
+// makePizza();
+// makePizza();
+// makePizza();
+
+// deel 2
+
+const makeSushi = function () {
+    console.log("Pak een zeewierblad");
+    console.log("Verdeel de sticky rice dun er overheen");
+    console.log("Leg er een dun plakje zalm op");
+    console.log("Voeg eventueel wasabi toe");
+    console.log("Maak voorzichtig met het blad een rol sushi");
+}
+
+// makeSushi();
+
+//restaurant 3x sushi en 2x pizza
+
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
